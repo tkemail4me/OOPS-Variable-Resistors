a0e1df513c1840fa32b84945326b1abbe674035b
tkemail4me <tkemail4me@gmail.com>
2025-09-18 16:49:17 -0400

    Add files via upload

--- resistor.py
+++ resistor.py
@@ -6,7 +6,7 @@
 # Tom Kalhous TPRG Fall 2025
 
 class Resistor():
-    def__init__(self, value, tol, rating):
+    def __init__(self, value, tol, rating):
         self.value = value
         self.tolerance = tol
         self.rating = rating
@@ -23,25 +23,25 @@ class Resistor():
         self.power = amperes**2 * self.value
     def parallel(self, other):
         return self.value * other.value / (self.value + other.value)
-    def__str__(self):
+    def __str__(self):
         return str(self.value) + " " + str(self.tolerance)
     
 class VariableResistor(Resistor):
-    def__init__(self, value, \
-                tol, rating, percent+100):
+    def __init__(self, value, \
+                tol, rating, percent = 100):
         super().__init__(value, tol, rating)
         self.percent = percent
         self.real_value = self.value
         self.value = self.real_value * self.percent / 100
-    def__str__(self):
-        return str(self.real_value) \
-               + "@" + str(self.percent) + "%" \
-               + " " + str(self.tolerance)
+    def __str__(self):
+        return str(self.real_value) + "@" + str(self.percent) + " = " \
+               + str(self.value) + "%" + " " + str(self.tolerance)
+
     
 if __name__ == "__main__":
     r1 = Resistor(220, 5, 1/4)
     r2 = Resistor(1000, 2, 1/8)
-    rv =
+    rv = VariableResistor(500, 2, 1/8, 33.3)
     print(r1)
     print(r2)
-    print(r3)
+    print(rv)
